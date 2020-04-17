2b3ef9d66234107d2b4995096fbbc7814419b942
NicoleBrown0477 <rinory1998@gmail.com>
2020-04-17 14:03:40 +0800

    Update index.php

--- index.php
+++ index.php
@@ -33,7 +33,7 @@ wp_reset_query();?>
 
             <div class="row">
 
-                <div class="article-title text-center wow fadeInLeft">
+                <div class="article-title text-center">
                     <h3>LASTEST ARTICLES</h3>
                 </div>
 
