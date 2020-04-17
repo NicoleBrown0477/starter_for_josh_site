7c2938dc395410057b9b67c6500e3b60c7d76175
NicoleBrown0477 <rinory1998@gmail.com>
2020-04-17 13:51:11 +0800

    Update home-page.php

--- home-page.php
+++ home-page.php
@@ -27,27 +27,30 @@
             <div class="row">
 
                 <div class="article-title text-center wow fadeInLeft">
-                    <h3>LASTEST ARTICLES</h3>
+                    <h3 class="text-center article-title "><?php the_field('titleforarticles'); ?></h3>
                 </div>
 
                 <div class="col-md-4 article-cent">
-
-<!--place holder for col1-->    
-                    
+<img class="img-responsive" alt=“portait” src="<?php the_field('articleimage'); ?>">
+<h4><?php the_field('articletitle'); ?></h4>
+<p><?php the_field('articleexcerpt'); ?> </p>
+<a href="<?php the_field('readmore'); ?>" class="readmore">CONTINUE READING</a>
 </div>
                 <!--col-md-4-->
 
                 <div class="col-md-4 article-cent">
-
-<!--place holder for col1-->
-                    
+<img class="img-responsive" alt=“portait” src="<?php the_field('articleimage2'); ?>">
+<h4><?php the_field('articletitle2'); ?></h4>
+<p><?php the_field('articleexcerpt2'); ?> </p>
+<a href="<?php the_field('readmore2'); ?>" class="readmore">CONTINUE READING</a>
 </div>
                 <!--col-md-4-->
 
                 <div class="col-md-4 article-cent">
-
-<!--place holder for col1-->                    
-                    
+<img class="img-responsive" alt=“portait” src="<?php the_field('articleimage3'); ?>">
+<h4><?php the_field('articletitle3'); ?></h4>
+<p><?php the_field('articleexcerpt3'); ?> </p>
+<a href="<?php the_field('readmore3'); ?>" class="readmore">CONTINUE READING</a>                  
 </div>
                 <!--col-md-4-->
 
